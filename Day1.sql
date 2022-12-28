ecb6ac70103643bbdfce38a7b60019f1dd89b365
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2022-12-28 10:30:16 +0530

    Update Day1.sql

--- Day1.sql
+++ Day1.sql
@@ -1,5 +1,5 @@
 #Basic Select
-#Revising the select query
+#Revising the select query I
 
 Q)Query all columns for all American cities in the CITY table with populations larger than 100000. 
   The CountryCode for America is USA.
