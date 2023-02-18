ed9480f8dca85a964d2c4ecf43fcb51e7ef7b137
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-02-18 13:24:33 +0530

    Update Day19.sql

--- Day19.sql
+++ Day19.sql
@@ -15,3 +15,37 @@ mysql> select * from participated;
 | 5d       | 5c    |      500 |       240000 |
 +----------+-------+----------+--------------+
 5 rows in set (0.00 sec)
+
+4)Add a new accident to database
+
+mysql> insert into accident values(600,'2021-09-23','agra');
+Query OK, 1 row affected (0.14 sec)
+
+mysql> insert into participated values('3d','3c',600,9000);
+Query OK, 1 row affected (0.13 sec)
+
+mysql> select * from accident;
++----------+------------+----------+
+| reportno | date       | location |
++----------+------------+----------+
+|      100 | 2002-06-12 | Kollam   |
+|      200 | 2020-06-12 | dhward   |
+|      300 | 2021-06-12 | Banglore |
+|      400 | 2009-06-12 | hubli    |
+|      500 | 2012-06-12 | gadag    |
+|      600 | 2021-09-23 | agra     |
++----------+------------+----------+
+6 rows in set (0.00 sec)
+
+mysql> select * from participated;
++----------+-------+----------+--------------+
+| driverid | regno | reportno | damageamount |
++----------+-------+----------+--------------+
+| 1d       | 1c    |      100 |        25000 |
+| 2d       | 2c    |      200 |        12300 |
+| 3d       | 3c    |      300 |         1200 |
+| 3d       | 3c    |      600 |         9000 |
+| 4d       | 4c    |      400 |        60000 |
+| 5d       | 5c    |      500 |       240000 |
++----------+-------+----------+--------------+
+6 rows in set (0.00 sec)
