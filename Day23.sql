c8f1f66af31216813df2f32e1150bba78231c43c
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-06-28 21:30:39 +0530

    Create Day23.sql

---
+++ Day23.sql
@@ -0,0 +1,15 @@
+#100DaysofSQL
+#Aggregation
+#CountFunction
+
+Task
+****
+Query a count of the number of cities in CITY having a Population larger than 100000.
+
+Query
+*****
+SELECT COUNT(*) FROM CITY WHERE CITY.POPULATION>100000;
+
+OUTPUT
+******
+6
